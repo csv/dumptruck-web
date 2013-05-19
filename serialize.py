7f7a8fbea43bf60242b9903558a6ddf5876980b4
Thomas Levine <occurrence@thomaslevine.com>
2013-05-19 11:10:29 -0700

    ?

--- serialize.py
+++ serialize.py
@@ -4,12 +4,17 @@ import StringIO
 
 def csvdump(data):
     'Dump a list of dictionaries to CSV.'
-    keys = data.keys()
-    output = StringIO.StringIO()
-    c = csv.DictWriter(f, keys)
-    c.writer.writerow(keys)
-    c.writerows(data)
-    return output.getvalue()
+    print json.dumps(data)
+    if len(data) == 0:
+        # It would be nice if this had the header row or raised an error or something.
+        return '\n'
+    else:
+        keys = data[0].keys()
+        output = StringIO.StringIO()
+        c = csv.DictWriter(output, keys)
+        c.writer.writerow(keys)
+        c.writerows(data)
+        return output.getvalue()
 
 SERIALIZATIONS = {
     'json': json.dumps,
